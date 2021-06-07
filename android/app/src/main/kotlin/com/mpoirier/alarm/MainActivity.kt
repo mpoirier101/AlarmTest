ebf0b9782ba48bc9f2b84fa477fbaa99158090b0
Michel Poirier <mpoirier101@gmail.com>
2021-06-07 11:09:34 -0400

    added permissions

---
+++ android/app/src/main/kotlin/com/mpoirier/alarm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mpoirier.alarm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- createGIT.cmd
+++ createGIT.cmd
@@ -4,3 +4,5 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/mpoirier101/AlarmTest.git
 git push -u origin main
+
+
